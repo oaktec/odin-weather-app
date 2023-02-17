4d1c8153b7e2d2eb26434c2354a41a4707d51963
oaktec <54672803+oaktec@users.noreply.github.com>
2023-02-17 21:26:05 +0000

    Implement basic display

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ async function getWeather(location) {
       `https://api.openweathermap.org/data/2.5/weather?q=${location.toLowerCase()}&APPID=3958700c4aae1e50012ae6d3204ebd41`
     );
     let data = await response.json();
-
     return interpretWeather(data);
   } catch (error) {
     console.log(error);
@@ -32,7 +31,17 @@ function validateSearch() {
     return false;
   }
   if (cityInput.value.match(/^[a-zA-Z]+$/)) return true;
+  alert("Please enter a valid city name");
   return false;
+}
+function presentWeather(weather) {
+  const cityName = document.querySelector("#city-name");
+  const weatherDescription = document.querySelector("#weather-description");
+  const temperature = document.querySelector("#temp");
+
+  cityName.textContent = `${weather.city}, ${weather.country}`;
+  weatherDescription.textContent = weather.description;
+  temperature.textContent = `${(weather.temperature - 273.15).toFixed(2)}°C`;
 }
 
 const cityInput = document.querySelector("#city-input");
@@ -43,7 +52,7 @@ searchButton.addEventListener("click", (e) => {
     return;
   }
   getWeather(cityInput.value).then((data) => {
-    console.log(data);
+    presentWeather(data);
   });
   e.preventDefault();
 });
